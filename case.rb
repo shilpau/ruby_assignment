6065297095dfef1e52131afaa513c95416cde9e1
shilpau <shilpau@cybage.com>
2013-04-25 18:38:29 +0530

    Create case.rb

---
+++ case.rb
@@ -0,0 +1,16 @@
+#!usr/bin/ruby
+
+puts "Enter a number"
+i= gets.to_i
+
+case i
+  when 1
+		puts "Its One"
+	when 2
+		puts "Its Two"
+	when 3
+		puts "Its Three"
+	else
+		puts "Does match any of the cases"
+end
+	
